5e0e642a433683a81ef15feaae3a1bc9543a2e76
Giovanni Pace <g.pace@braintechinnovations.it>
2025-07-10 16:05:16 +0200

    Arrow

---
+++ 00_Introduzione/07_metodi.js
@@ -0,0 +1,12 @@
+// function saluta(varNome){
+//     console.log("CIAO " + varNome)
+// }
+
+
+const saluta = (varNome) => {
+    console.log("CIAO " + varNome)
+}
+
+saluta("Giovanni")
+saluta("Valeria")
+saluta("Marika")
