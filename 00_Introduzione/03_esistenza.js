071636f83ea267f589b5c8fdc0ebdd6b81bc4271
Giovanni Pace <g.pace@braintechinnovations.it>
2025-07-10 15:43:10 +0200

    Aggiunti controllo

---
+++ 00_Introduzione/03_esistenza.js
@@ -0,0 +1,14 @@
+let prima;
+// console.log(prima);
+
+let seconda = null;
+// console.log(seconda);
+
+let terza = false;
+
+let quarta = 0;
+
+if(quarta)
+    console.log("Sono stato dichiarato");
+else
+    console.log("Non sono stato dichiarato")
